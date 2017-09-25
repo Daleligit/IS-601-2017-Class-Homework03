893b370bcf72b9263070be791b8f990f139841d4
Yi Li <yl622@njit.edu>
2017-09-25 11:43:24 -0400

    built the basic structure of index.php and added it

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+  
+  $obj = new main();
+
+  class main {
+    public function __construct () {
+      echo 'This is the homework 03 for creating examples of  10 string and 10 
+      array functions </br>';
+
+    }
+    public function __destruct () {
+      echo '</br> I\'m Done';
+    }
+  }
+
+  
+?>
