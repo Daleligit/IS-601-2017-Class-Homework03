d5c72c5fa75c694cfc686c5468afc722e861703c
Yi Li <yl622@njit.edu>
2017-09-25 11:55:23 -0400

    added string print function in index

--- index.php
+++ index.php
@@ -4,14 +4,26 @@
 
   class main {
     public function __construct () {
-      echo 'This is the homework 03 for creating examples of  10 string and 10 
-      array functions </br>';
-
+      echo '<i>This is the homework 03 for creating examples of  10 string and 10 
+      array functions</i></br>';
+      $stringText = "<h1>10 String Functions:</h3>";
+      stringFunctions::printThis ($stringText);
+      $stringText = "<h3>Print string function:</h3>";
+      stringFunctions::printThis ($stringText);
+      $stringText = "<p>This is my string example</p>";
+      stringFunctions::printThis ($stringText);
+      $stringText = "<hr />";
+      stringFunctions::printThis ($stringText);
     }
     public function __destruct () {
       echo '</br> I\'m Done';
     }
   }
 
-  
+  class stringFunctions {
+    static public function printThis ($text) {
+      print($text);
+    }
+  }
+
 ?>
