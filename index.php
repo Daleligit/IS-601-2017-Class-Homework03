ee965a131f1c723c066521185a6e5c888c6783f7
Yi Li <yl622@njit.edu>
2017-09-25 12:12:30 -0400

    added string length function in index

--- index.php
+++ index.php
@@ -6,11 +6,23 @@
     public function __construct () {
       echo '<i>This is the homework 03 for creating examples of  10 string and 10 
       array functions</i></br>';
+      // 10 string functions
       $stringText = "<h1>10 String Functions:</h3>";
       stringFunctions::printThis ($stringText);
+      // 1st
       $stringText = "<h3>Print string function:</h3>";
       stringFunctions::printThis ($stringText);
-      $stringText = "<p>This is my string example</p>";
+      $stringText = "This is my string example";
+      stringFunctions::printThis ($stringText);
+      $stringText = "<hr />";
+      stringFunctions::printThis ($stringText);
+      // 2nd
+      $stringText = "<h3>Get string length function:</h3>";
+      stringFunctions::printThis ($stringText);
+      $stringText = "This is my string example";
+      stringFunctions::printThis ($stringText);
+      $stringLen = stringFunctions::getStringLen($stringText);
+      $stringText = "</br>String length = $stringLen";
       stringFunctions::printThis ($stringText);
       $stringText = "<hr />";
       stringFunctions::printThis ($stringText);
@@ -21,8 +33,12 @@
   }
 
   class stringFunctions {
-    static public function printThis ($text) {
-      print($text);
+    static public function printThis ($input) {
+      print($input);
+    }
+
+    static public function getStringLen ($input) {
+      return(strlen($input));
     }
   }
 
