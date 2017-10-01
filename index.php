de2b724a90c744b631ef1840a7f6cd319495fb81
Yi Li <yl622@njit.edu>
2017-10-01 17:26:07 -0400

    fixed 5 array functions

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
     public function __construct () {
       
       $this->html .= htmlTags::changeRow (htmlTags::styleItalic ('This is the homework 03 for creating examples of  10 string and 10 array functions'));
-      // 10 string functions
       
+      // 10 string functions
       $stringText = 'This is my string example';
       $arrayExample = array('This','is','my','array','example');
       $this->html .= htmlTags::headingOne ('10 String Functions:');
@@ -75,53 +75,38 @@
       $this->html .= htmlTags::changeRow ('I would like to have a substring start at position ' . $substringStart . ' with the length of ' . $substringLen);
       $this->html .= htmlTags::changeRow (stringFunctions::getSubstring ($stringText,$substringStart,$substringLen));
       $this->html .= htmlTags::horizontalRule ();
-      print ($this->html);
+
       //10 array functions
-      $stringText = "<h1>10 array Functions:</h3>";
-      stringFunctions::printThis ($stringText);
+      $arrayInput = array('a','c','b',22,6,'a');
+      $this->html .= htmlTags::headingOne ('10 array Functions:');
       //1st
-      $stringText = "<h3>Split function</h3>";
-      stringFunctions::printThis ($stringText);
-      $arrayInput = array('a','c','b',22,6,'a');
-      arrayFunctions::printThis ($arrayInput);
+      $this->html .= htmlTags::headingThree ('Split function');
+      $this->html .= htmlTags::changeRow (arrayFunctions::printThis ($arrayInput));
       $splitSize = 2;
-      $stringText = "</br>I would like to splite the array with size $splitSize";
-      stringFunctions::printThis ($stringText);
-      arrayFunctions::arraySplit ($arrayInput,$splitSize);
-      $stringText = "<hr />";
-      stringFunctions::printThis ($stringText);
+      $this->html .= htmlTags::changeRow ('I would like to splite the array with size' . $splitSize);
+      $this->html .= htmlTags::changeRow (arrayFunctions::printThis (arrayFunctions::arraySplit ($arrayInput,$splitSize)));
+      $this->html .= htmlTags::horizontalRule ();
       //2nd
-      $stringText = "<h3>Counts all values function</h3>";
-      stringFunctions::printThis ($stringText);
-      $arrayInput = array('a','c','b',22,6,'a');
-      arrayFunctions::printThis ($arrayInput);
-      arrayFunctions::countVal ($arrayInput);
-      $stringText = "<hr />";
-      stringFunctions::printThis ($stringText);
+      $this->html .= htmlTags::headingThree ('Counts all values function');
+      $this->html .= htmlTags::changeRow (arrayFunctions::printThis ($arrayInput));
+      $this->html .= htmlTags::changeRow (arrayFunctions::printThis (arrayFunctions::countVal ($arrayInput)));
+      $this->html .= htmlTags::horizontalRule ();
       //3rd
-      $stringText = "<h3>Count elements function</h3>";
-      stringFunctions::printThis ($stringText);
-      $arrayInput = array('a','c','b',22,6,'a');
-      arrayFunctions::printThis ($arrayInput);
-      arrayFunctions::countEle ($arrayInput);
-      $stringText = "<hr />";
-      stringFunctions::printThis ($stringText);
+      $this->html .= htmlTags::headingThree ('Count elements function');
+      $this->html .= htmlTags::changeRow (arrayFunctions::printThis ($arrayInput));
+      $this->html .= htmlTags::changeRow (arrayFunctions::countEle ($arrayInput));
+      $this->html .= htmlTags::horizontalRule ();
       //4th
-      $stringText = "<h3>Sort in reverse order function</h3>";
-      stringFunctions::printThis ($stringText);
-      $arrayInput = array('a','c','b',22,6,'a');
-      arrayFunctions::printThis ($arrayInput);
-      arrayFunctions::arrayRsort ($arrayInput);
-      $stringText = "<hr />";
-      stringFunctions::printThis ($stringText);
+      $this->html .= htmlTags::headingThree ('Sort in reverse order function');
+      $this->html .= htmlTags::changeRow (arrayFunctions::printThis ($arrayInput));
+      $this->html .= htmlTags::changeRow (arrayFunctions::printThis (arrayFunctions::arrayRsort ($arrayInput)));
+      $this->html .= htmlTags::horizontalRule ();
       //5th
-      $stringText = "<h3>Sort function</h3>";
-      stringFunctions::printThis ($stringText);
-      $arrayInput = array('a','c','b',22,6,'a');
-      arrayFunctions::printThis ($arrayInput);
-      arrayFunctions::arraySort ($arrayInput);
-      $stringText = "<hr />";
-      stringFunctions::printThis ($stringText);
+      $this->html .= htmlTags::headingThree ('Sort in reverse order function');
+      $this->html .= htmlTags::changeRow (arrayFunctions::printThis ($arrayInput));
+      $this->html .= htmlTags::changeRow (arrayFunctions::printThis (arrayFunctions::arraySort ($arrayInput)));
+      $this->html .= htmlTags::horizontalRule ();
+      print ($this->html);
       //6th
       $stringText = "<h3>Sum function</h3>";
       stringFunctions::printThis ($stringText);
@@ -178,30 +163,25 @@
     }
 
     static public function arraySplit ($input,$input2) {
-      print("</br>");
-      print_r(array_chunk($input,$input2));
+      return array_chunk($input,$input2);
     }
 
     static public function countVal ($input) {
-      print("</br>");
-      print_r(array_count_values($input));
+      return array_count_values($input);
     }
 
     static public function countEle ($input) {
-      print("</br>");
-      print_r(count($input));
+      return count($input);
     }
 
     static public function arrayRsort ($input) {
-      print("</br>");
       rsort($input);
-      print_r($input);
+      return $input;
     }
 
     static public function arraySort ($input) {
-      print("</br>");
       sort($input);
-      print_r($input);
+      return $input;
     }
     
     static public function arraySum ($input) {
